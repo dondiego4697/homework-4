324d032a34eb19cc628f4c88d14dd4f18e7d6d5a
Sovianum <aklyukvin@yandex.com>
2017-11-26 17:14:42 +0300

    remove not used method

--- tests/Page/Page.py
+++ tests/Page/Page.py
@@ -14,7 +14,3 @@ class Page(object):
         url = urlparse.urljoin(self.BASE_URL, self.PATH)
         self.driver.get(url)
         self.driver.maximize_window()
-
-    def scroll_down(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(0.5)
