01da1e72eea7357483c6600ad7cdac37d6f2cab8
Sovianum <aklyukvin@yandex.com>
2017-11-28 07:08:48 +0300

    fix anvironment for post_manipulation_tests

--- tests/post_manipulation_tests.py
+++ tests/post_manipulation_tests.py
@@ -4,16 +4,24 @@ from tests.main import Tests
 
 
 class PostManipulationTests(Tests):
+    def setUp(self):
+        super(PostManipulationTests, self).setUp()
+        self._post_string('msg', False)
+
     def test_post_delete(self):
-        main_page = self._to_main_page()
-        post = main_page.get_last_post()
+        profile_page = self._to_profile_page()
+        post = profile_page.get_last_post()
         post.delete()
         self.assertTrue(post.is_deleted())
 
+        self._delete_last_post_from_notes()
+
     def test_post_add_to_bookmarks(self):
-        main_page = self._to_main_page()
-        post = main_page.get_last_post()
+        profile_page = self._to_profile_page()
+        post = profile_page.get_last_post()
         post_frame = post.open_post_frame()
         post_frame.add_post_to_bookmarks()
         self.assertTrue(post_frame.is_added_to_bookmarks())
+
+        self._delete_last_post_from_notes()
 
